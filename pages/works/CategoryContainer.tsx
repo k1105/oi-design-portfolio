03da64c9cf47e4d503fc7cdb9751497c2b7c828f
k1105 <kanapon1105@yahoo.co.jp>
2024-06-22 15:44:20 +0900

    選択されているカテゴリ名のスタイルを変更

--- pages/works/CategoryContainer.tsx
+++ pages/works/CategoryContainer.tsx
@@ -4,7 +4,7 @@ import { useCategory } from "./CategoryContext";
 
 const CategoryContainer = () => {
   const [data, setData] = useState<Category[] | null>(null);
-  const { setSelectedCategory } = useCategory();
+  const { selectedCategory, setSelectedCategory } = useCategory();
 
   useEffect(() => {
     async function fetchData() {
@@ -24,7 +24,11 @@ const CategoryContainer = () => {
       <div className="category-container">
         {data &&
           data.map((category, index) => (
-            <p key={index} onClick={() => handleCategoryClick(category)}>
+            <p
+              key={index}
+              onClick={() => handleCategoryClick(category)}
+              className={selectedCategory?.id === category.id ? "selected" : ""}
+            >
               {category.name}
             </p>
           ))}
@@ -50,7 +54,8 @@ const CategoryContainer = () => {
           cursor: pointer;
         }
 
-        p:hover {
+        p:hover,
+        p.selected {
           background-color: var(--light-yellow);
           color: white;
         }
