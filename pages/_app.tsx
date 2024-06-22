cb6a584734fea8a7c9a6d91aa42e0b04aefc28cc
k1105 <kanapon1105@yahoo.co.jp>
2024-06-22 16:21:08 +0900

    dynamic-importの削除

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { CategoryProvider } from "../components/CategoryContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <CategoryProvider>
-      <Component {...pageProps} />
-    </CategoryProvider>
-  );
+  return <Component {...pageProps} />;
 }
