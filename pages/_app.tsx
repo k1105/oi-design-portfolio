73fc30c83990a6bc69cb2be6d17cf17ebe753eb3
k1105 <kanapon1105@yahoo.co.jp>
2024-06-22 16:11:48 +0900

    CategoryProviderを_appでimport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { CategoryProvider } from "./works/CategoryContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CategoryProvider>
+      <Component {...pageProps} />
+    </CategoryProvider>
+  );
 }
